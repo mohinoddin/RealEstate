09a15ebb44643c6287e1f1a54dcad1ad1534f25a
mohinoddin <mohinoddin80@gmail.com>
2022-07-27 20:38:46 +0530

    some changes

--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -1,8 +1,9 @@
 const Signup = ()=>{
     return(
         <>
-       <div className="p-3 mb-2 bg-info text-dark">sign up works</div>
-        using from vs code
+       
+
+        <div>one more div</div>
         </>
     )
 }
