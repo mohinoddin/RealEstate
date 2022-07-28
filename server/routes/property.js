15e94bcf0d4f8431ea3bc7017aef6eb5a52f324c
mohinoddin <mohinoddin80@gmail.com>
2022-07-28 17:10:50 +0530

    list property commit

--- server/routes/property.js
+++ server/routes/property.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const jwt = require("jsonwebtoken")
 const router = express.Router();
-const signupModal = require("../modal/signup-Modal")
+const signupModal = require("../models/signup-Modal")
 
 
 router.get("/", (req, res) => {
