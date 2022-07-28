2afc60eda8d080939d9004a6fd21fc9dbacd7868
thrinath <thrinathsunny99@gmail.com>
2022-07-28 19:59:30 +0530

    howowow

--- client/src/components/signup.js
+++ client/src/components/signup.js
@@ -2,7 +2,8 @@ const Signup = ()=>{
     return(
         <>
        <div className="p-3 mb-2 bg-info text-dark">sign up works</div>
-        using from vs code
+        using from vs code\
+        lkokikkokkk
 
         </>
     )
