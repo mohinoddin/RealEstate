bee8793540beda64ff3eb993ccd565994a268695
mohinoddin <mohinoddin80@gmail.com>
2022-07-28 11:42:02 +0530

    list propert commit

---
+++ server/routes/property.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+const router = express.Router();
+
+router.get("/getAllProperty", (req, res) => {
+
+    // propertyModal.find().sort({ _id: -1 }).then((propertyData) => {
+    //     res.status(200).send(propertyData)
+    // })
+
+    console.log("get property list works")
+    res.status(200).send("get property list works")
+})
+
+
+router.get("/getProperty/:id", (req, res) => {
+
+    //     propertyModal.find(_id).then((propertyData) => {
+    //        res.status(200).send(propertyData)
+    //    })
+
+    console.log("get property by id works")
+    res.status(200).send("get property by id works")
+})
+
+
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,29 @@
-const express = require("express")
-const app= express()
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+// const propertyModal = require('./modals/propertSchema')
+const property=require("./routes/property")
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(3001,(err)=>{
-    if(!err){
+
+app.listen(3001, (err) => {
+    if (!err) {
         console.log("server started")
-    }else{
+    } else {
         console.log(err)
     }
 })
 
+const mongoDB = "mongodb+srv://RealEstate:RealEstate@cluster0.gdir3.mongodb.net/property_dtls?retryWrites=true&w=majority";
+mongoose.connect(mongoDB, {}).then((res) => {
+    console.log("connected to db")
+}).catch((err) => {
+    console.log(err)
+})
+
+
+app.get('/',(req,res)=>{
+  res.send("base route")
+})
+app.use("/getProperty",property)
