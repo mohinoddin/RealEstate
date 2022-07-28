0864f0c6a5114ca3f9c6cb9937fa588273c88a9f
mohinoddin <mohinoddin80@gmail.com>
2022-07-28 16:51:26 +0530

    list property commit

--- server/routes/property.js
+++ server/routes/property.js
@@ -1,55 +1,76 @@
 const express = require("express");
 const app = express();
-
+const jwt = require("jsonwebtoken")
 const router = express.Router();
+const signupModal = require("../modal/signup-Modal")
 
-router.get("/", (req, res) => {   
-   
-    // if(req.headers.authorization){
 
-    //     try{
-    //         user_mail = jwt.verify(req.headers.authorization, process.env.SECRET_KEY);
-    //         propertyModal.find().then((propertyData) => {
-    //             res.status(200).send(propertyData)
-    //         })
-        
-    //     }catch(err){
-           
-    //             res.status(403).send("User not authorized")
-           
-    //     }
+router.get("/", (req, res) => {
 
-    // }else{
+    if (req.headers.authorization) {
 
-    // }
+        try {
+            user_mail = jwt.verify(req.headers.authorization, process.env.SECRET_KEY);
+            signupModal.find({ email: user_mail }).then((userData) => {
 
-    console.log("get property list works")
-    res.status(200).send("get property list works")
+                if (userData.length) {
+                    console.log(user_mail)
+                    res.status(200).send("get property list works")
+
+                    //          propertyModal.find().then((propertyData) => {
+                    //     res.status(200).send(propertyData)
+                    // })
+
+                } else {
+                    res.status(403).send("No such user exist with the mentioned email id")
+                }
+
+            }).catch((err) => {
+                res.status(403).send(err.message)
+            })
+        } catch (err) {
+            res.status(403).send("User not authorized", err)
+        }
+
+    } else {
+        res.status(200).send("header is empty please add header")
+    }
+
+
 })
 
 
 router.get("/:id", (req, res) => {
 
-// if(req.headers.authorization){
+    if (req.headers.authorization) {
 
-    //     try{
-    //         user_mail = jwt.verify(req.headers.authorization, process.env.SECRET_KEY);
-    //         propertyModal.find(_id).then((propertyData) => {
-    //             res.status(200).send(propertyData)
-    //         })
-        
-    //     }catch(err){
-           
-    //             res.status(403).send("User not authorized")
-           
-    //     }
+        try {
+            user_mail = jwt.verify(req.headers.authorization, process.env.SECRET_KEY);
+            signupModal.find({ email: user_mail }).then((userData) => {
 
-    // }else{
+                if (userData.length) {
+                    console.log(user_mail)
+                    res.status(200).send("get property list works")
 
-    // }
+                    //          propertyModal.find(_id).then((propertyData) => {
+                    //     res.status(200).send(propertyData)
+                    // })
 
-    console.log("get property by id works")
-    res.status(200).send("get property by id works")
+                } else {
+                    res.status(403).send("No such user exist with the mentioned email id")
+                }
+
+            }).catch((err) => {
+                res.status(403).send(err.message)
+            })
+        } catch (err) {
+            res.status(403).send("User not authorized", err)
+        }
+
+    } else {
+        res.status(200).send("header is empty please add header")
+    }
+
 })
 
 
